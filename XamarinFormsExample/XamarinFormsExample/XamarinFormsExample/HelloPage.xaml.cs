92beaa92ce5f47c37938868cee59d80f59b5c4b9
BenjaminRomano <bromano@crimson.ua.edu>
2015-04-04 00:16:46 -0500

    Initial Commit of resources for ACM Talk

---
+++ XamarinFormsExample/XamarinFormsExample/XamarinFormsExample/HelloPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinFormsExample
+{
+    public partial class HelloPage : ContentPage
+    {
+        public HelloPage()
+        {
+            InitializeComponent();
+
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            var response = await DisplayAlert("Print Hello", "Are you sure?", "Print", "Cancel");
+
+            if (response)
+            {
+                this.HelloLabel.Text = "Hello, " + this.NameEntry.Text;
+            }
+            else
+            {
+                this.HelloLabel.Text = String.Empty;
+            }
+        }
+    }
+}
